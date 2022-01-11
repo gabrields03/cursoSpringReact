dd501bb461a5adadc0c16c0f412a5861118b492a
Gabriel <gabrielsaojoao12@gmail.com>
2022-01-10 21:08:06 -0300

    Project created

---
+++ backend/src/main/java/com/gabrields/cursoSpringReact/CursoSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.gabrields.cursoSpringReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringReactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gabrields/cursoSpringReact/CursoSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrields.cursoSpringReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
