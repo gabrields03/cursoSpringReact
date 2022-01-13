b3e1152d8c3c7942ae821231c027070831cdabfc
Gabriel <gabrielsaojoao12@gmail.com>
2022-01-13 13:35:38 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './styles.css';
+
+export default function Navbar(){
+return(
+<header>
+    <nav className="container">
+        <div className="dsmovie-nav-content">
+            <h1>DsMovie</h1>
+            <a href="https://github.com/gabrields03">
+                <div className="dsmovie-contact-container">
+                    <GithubIcon />
+                    <p className="dsmovie-contact-link">/gabrields03</p>
+                </div>
+            </a>
+        </div>
+    </nav>
+</header>
+);
+}
