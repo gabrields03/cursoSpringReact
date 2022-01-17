85a622829a7384dc585a3ea707829f28a2015481
Gabriel <gabrielsaojoao12@gmail.com>
2022-01-17 00:07:39 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,38 +1,53 @@
 import axios from "axios";
-import Pagination from 'components/Pagination';
-import MovieCard from 'components/MovieCard';
+import MovieCard from "components/MovieCard";
+import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
-export default function Listing(){
+export default function Listing() {
+    const [pageNumber, setPageNumber] = useState(0);
 
-    //FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(response => {
-        console.log(response.data);
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
     });
-return(
-<>
-    <Pagination />
-    <div className="container">
-        <div className="row">
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard />
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard />
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard />
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard />
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard />
-            </div>
-        </div>
-    </div>
 
-</>
-);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+            });
+    }, [pageNumber]);
+
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
+
+    return (
+        <>
+            <Pagination page={page} onChange={handlePageChange}/>
+            <div className="container">
+                <div className="row">
+                    {page.content.map(movie =>    (
+                            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                                <MovieCard movie = {movie} />
+                            </div>
+                        )
+                    )}
+
+                </div>
+
+            </div>
+        </>
+
+    );
 }
